7cd04de548bc7f66b30af3f594db3bc769bfe64b
ProjectTPP <tyrykin.yaa@edu.spbstu.ru>
2024-04-29 22:55:19 +0300

    Fixes for dataset creation - added option of "black-white" images producing

--- image_processing/image_creation.py
+++ image_processing/image_creation.py
@@ -6,15 +6,6 @@ import multiprocessing as mp
 
 THREADS_NUMBER = 20  # пока такое значение, чтобы машина не померла
 
-
-def get_column_interval(column_values: list) -> list:
-    return [min(column_values), max(column_values)]
-
-
-def scale_value(value: float, old_borders: list, new_borders: list) -> float:
-    return (new_borders[1] - new_borders[0]) / (old_borders[1] - old_borders[0]) * (value - old_borders[0])
-
-
 def weather_to_pixel(value: float) -> dict:
     """
     Принцип перевода погодных факторов в пиксели результирующего искусственного изображения по 3 цветовым каналам
@@ -31,44 +22,20 @@ def weather_to_pixel(value: float) -> dict:
     return {"r": r, "g": g, "b": b}
 
 
-def snp_to_pixel_rgb(value: float, interval: list) -> dict:  # в интервале должно быть 3 значения: [0, 1, 2] или [-1, 0, 1]
+def snp_to_pixel_bw(value: int, max_allels: float) -> float:
     """
-    Принцип перевода в интенсивность пикселя: есть 2 односторонних интервала - [ interval[0], interval[1] ) и
-    [ interval[1], interval[2] ). В случае если признк принимает значение на границах интервалов (значений SNP),
-    то при этом задается максимальная интенсивность одного из трех каналов, отвечающего за интенсивность цветов в
-    пикселе. Если значение промежуточное (только для канал red и green), то значение шкалируется с интервала исходных
-    значений на интервал значений пикселя [0, 255].
+    Функция перевода значения снипа в интенсивность черно-белого пикселя. Черным пиксель становится, если
+    соответствующее поле имеет пропуск данных (NA), наиболее светлый - при максимальном номере аллели среди
+    всех имеющихся.
 
-    :param value: начальное значения признака у образца
-    :param interval: интервал значений признака среди всех имеющихся образцов
-
-    :return: [r, g, b] - массив значений интенсивностей цветовых каналов
+    :param value: числовая метка аллели для генетического маркера
+    :param max_allels: максимальное число аллелей по всем генетическим маркерам датасета
+    :return: Значение соответствующего пикселя
     """
-
-    r, g, b = 0, 0, 0
-    if interval[0] <= value < interval[1]:  # -1 <= val < 0 или 0 <= val < 1
-        if value == interval[0]:
-            r = 255
-        else:
-            r = scale_value(value, [interval[0], interval[2]], [0, 255])
-    elif value == interval[1]:  # 0 <= val < 1 или 1 <= val < 2
-        if value == interval[0]:
-            g = 255
-        else:
-            g = scale_value(value, [interval[0], interval[2]], [0, 255])
-    else:  # val == 2 или val == 1
-        b = 255
-    return {"r": r, "g": g, "b": b}
-
-
-def snp_to_pixel_bw(value: int) -> dict:
-    """
-    Функция перевода значения снипа в интенсивность пикселя. В данном случае пиксели должны быть черно-белыми, поэтому
-    интервал
-    :param value:
-    :return:
-    """
-    pass
+    if np.isnan(value):
+        return 0.0
+    else:
+        return 255.0 / (max_allels + 1) * (value + 1)
 
 
 def snp_weather_colnames(all_colnames: list, meteo_names: list) -> list:
@@ -96,7 +63,7 @@ def snp_weather_colnames(all_colnames: list, meteo_names: list) -> list:
 
 
 def create_image_dataset(df: pd.DataFrame, n_rows: int, n_columns: int, snp_columns: list, weather_columns: list,
-                         original_image_path: str = "", view_image_path: str = "", multithread: bool=False) -> list:
+                         for_model_path: str = "", for_user_path: str = "", multithread: bool=False) -> list:
     """
     Основная функция формирования датасета искусственных изображений для обучения и тестирования модели.
 
@@ -105,12 +72,14 @@ def create_image_dataset(df: pd.DataFrame, n_rows: int, n_columns: int, snp_colu
     :param n_columns: Число строк пикселей в результирующем изображении (которое используется именно в модели)
     :param snp_columns: Список имен генетических столбцов-признаков, которые есть в датасете
     :param weather_columns: Список имен погодных столбцов-признаков, которые есть в датасете
-    :param original_image_path: Путь для сохранения датасета изображений в удобном для использования в модели формате
-    :param view_image_path: Путь для сохранения датасета изображений в удобном для визуального анализа формате
+    :param for_model_path: Путь для сохранения датасета изображений в удобном для использования в модели формате
+    :param for_user_path: Путь для сохранения датасета изображений в удобном для визуального анализа формате
     :param multithread: Логический флаг, определяющий пользователем необходимость распараллелить вычисления
 
     :return: Список объектов синтетических изображений в классе Image библиотеки Pillow
     """
+
+    max_allel = max_allels_number(df)
 
     images = []
     if multithread:
@@ -119,24 +88,24 @@ def create_image_dataset(df: pd.DataFrame, n_rows: int, n_columns: int, snp_colu
             for i in df.index:
                 plant = df.iloc[i]
                 threads_data.append((plant, n_rows, n_columns, snp_columns, weather_columns,
-                                     original_image_path + "/" + "AIO_" + str(i + 1) + ".png",
-                                     view_image_path + "/" + "AIO_view_" + str(i + 1) + ".png", i))
+                                     for_model_path + "/" + "AIO_" + str(i + 1) + ".png",
+                                     for_user_path + "/" + "AIO_view_" + str(i + 1) + ".png", max_allel, i))
 
             images = aio_process_pool.starmap(process_single_image, threads_data)
     else:
         for i in df.index:
             plant = df.loc[i]
             plant_img = process_single_image(plant, n_rows, n_columns, snp_columns, weather_columns,
-                                             original_image_path + "/" + "AIO_" + str(i + 1) + ".png"
-                                             if original_image_path else "",
-                                             view_image_path + "/" + "AIO_view_" + str(i + 1) + ".png"
-                                             if view_image_path else "", i)
+                                             for_model_path + "/" + "AIO_" + str(i + 1) + ".png"
+                                             if for_model_path else "",
+                                             for_user_path + "/" + "AIO_view_" + str(i + 1) + ".png"
+                                             if for_user_path else "", max_allel, i)
             images.append(plant_img)
     return images
 
 
 def create_aio_for_plant(row: pd.DataFrame, rows: int, cols: int, snp_columns: list,
-                         weather_columns: list, ind: int=0) -> Image:
+                         weather_columns: list, max_allel: float) -> Image:
     """
     Функция непосредственно для создания AIO для заданного образца
 
@@ -148,9 +117,10 @@ def create_aio_for_plant(row: pd.DataFrame, rows: int, cols: int, snp_columns: l
 
     :return: объект класса Image пакета Pillow, содержащий искусственное изображение
     """
+
     # if rows * cols != row.shape[0]:
     #     raise Exception("Incorrect dimensions of AIO_set and input dataset cell")
-    array_red, array_green, array_blue = np.zeros((rows, cols)), np.zeros((rows, cols)), np.zeros((rows, cols))
+    arr = np.zeros((rows, cols))
 
     for i in range(rows):
         for j in range(cols):
@@ -158,48 +128,71 @@ def create_aio_for_plant(row: pd.DataFrame, rows: int, cols: int, snp_columns: l
             if idx < len(snp_columns):
                 # interval = get_column_interval(total_df[snp_columns[i * cols + j]])
                 val = row[snp_columns[idx]]
-                rgb = snp_to_pixel(val, [0, 1, 2])
+                bw = snp_to_pixel_bw(val, max_allel)
             else:
                 val = row[weather_columns[idx - len(snp_columns)]]
-                rgb = weather_to_pixel(val)
-            array_red[i, j] = rgb["r"]
-            array_green[i, j] = rgb["g"]
-            array_blue[i, j] = rgb["b"]
+                bw = weather_to_pixel(val)
+            arr[i, j] = bw
 
-    red_img = Image.fromarray(array_red).convert("L")
-    green_img = Image.fromarray(array_green).convert("L")
-    blue_img = Image.fromarray(array_blue).convert("L")
-    img = Image.merge("RGB", (red_img, green_img, blue_img))
+    img = Image.fromarray(np.uint8(arr), 'L')
     return img
 
 
-def process_single_image(row: pd.DataFrame, rows: int, cols: int, snp_columns: list, weather_columns: list,
-                         original_image_path: str = "", view_image_path: str = "", idx: int = 0) -> Image:
+def process_single_image(row: pd.DataFrame, width: int, height: int, snp_columns: list, weather_columns: list,
+                         for_model_path: str = "", for_user_path: str = "", max_allel: float = 1.0,
+                         idx: int = 0) -> Image:
     """
     Метод, выполняющий создание/сохранение синтетического изображения в двух форматах -
     непосредственно для модели и для визуального просмотра исследователем. В основном написан для
     распараллеливания создания AIO (экономия времени).
 
     :param row: растение-образец из общей таблицы данных, который требуется обработать
-    :param rows: число пикселей в столбце результирующего синтетического изображения
-    :param cols: число пикселей в ряду результирующего синтетического изображения
+    :param width: число пикселей в столбце результирующего синтетического изображения
+    :param height: число пикселей в ряду результирующего синтетического изображения
     :param snp_columns: названия колонок генетических факторов
     :param weather_columns: названия колонок погодных факторов
-    :param original_image_path: каталог, в который требуется сохранять изображения для использования моделью
-    :param view_image_path: каталог, в который требуется сохранять изображения для использования моделью
+    :param for_model_path: каталог, в который требуется сохранять изображения для использования моделью
+    :param for_user_path: каталог, в который требуется сохранять изображения для использования моделью
+    :param max_allel: максимальное число аллелей среди всех генетических маркером имеющихся
     :param idx: индекс для отладочной информации в командную строку - номер образца в общем датасете
 
     :return: объект искусственного изображения для данного образца в виде класса Image
     """
 
-    plant_img = create_aio_for_plant(row, rows, cols, snp_columns, weather_columns)
+    plant_img = create_aio_for_plant(row, width, height, snp_columns, weather_columns, max_allel)
     base_width, base_height = 600, 600  # required width and height of input images (for visual estimation)
     view_img = plant_img.resize((base_width, base_height), Image.Resampling.LANCZOS)
-    if view_image_path:  # сохранение изображений для визуальной оценки
-        view_img.save(view_image_path)
-    if original_image_path:  # сохранение изображений непосредственно для модели
-        plant_img.save(original_image_path)
+    if for_user_path:  # сохранение изображений для визуальной оценки
+        view_img.save(for_user_path)
+    if for_model_path:  # сохранение изображений непосредственно для модели
+        plant_img.save(for_model_path)
     print(f"Image #{idx + 1} processed")
     return plant_img
 
 
+def max_allels_number(df: pd.DataFrame) -> float:
+    """
+    Функция поиска максимального количества аллелей среди всех имеющихся генетических маркеров для дальнейшего
+    преобразования признаков аллелей в числовые метки, а следом и в значения пикселя в градациях серого
+
+    :param df: датасет с генетическими данным, который необходимо исследовать
+    :return: Максимум аллелей для одного генетического маркера среди всех имеющихся снипов
+    """
+
+    total_max = 1.0
+    for col in df:
+        temp_max = max(df[col])
+        if temp_max > total_max:
+            total_max = temp_max
+    return total_max
+
+
+print("DF#1")
+df_1 = pd.read_csv("../datasets/wheat/wheat_pheno_num_sync.csv")
+df_1 = df_1.drop(["Unnamed: 0", "number"], axis=1)
+
+print("DF#2")
+df_2 = pd.read_csv("../datasets/wheat/markers_poly_filtered_sync.csv")
+df_2 = df_2.drop(["Unnamed: 0"], axis=1)
+
+create_image_dataset(df_2, 128, 128, df_2.columns, [], "../AIO_set_wheat/for_model", "../AIO_set_wheat/for_user")
