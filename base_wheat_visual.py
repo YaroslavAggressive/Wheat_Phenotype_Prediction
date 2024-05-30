2ac2c50fdd92595f11c08daaba7ba27e04d328b5
ProjectTPP <tyrykin.yaa@edu.spbstu.ru>
2024-05-30 22:22:02 +0300

    Update plotting script

--- base_wheat_visual.py
+++ base_wheat_visual.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from support.phenotype_normalization import rank_based_transform, data_standardization
+
 # загрузка исходных данныъ
 name_1 = "datasets/wheat/markers_poly_filtered_sync.csv"
 name_2 = "datasets/wheat/wheat_pheno_num_sync.csv"
@@ -15,15 +17,15 @@ print(df_2.columns)
 # урожайность зерна (в граммах) и высоту растений (в см)
 
 # урожайность
-crop_yeild = df_2["Урожайность.зерна..г."]
+crop_yield = df_2["Урожайность.зерна..г."]
 fig, ax = plt.subplots()
-plt.plot(list(range(crop_yeild.shape[0])), list(crop_yeild), '.')
+plt.plot(list(range(crop_yield.shape[0])), list(crop_yield), '.')
 plt.xlabel("Номер образца")
 plt.ylabel("Урожайность зерна (г)")
 plt.title("Показатель урожайности зерна исследованных образцов")
 plt.grid()
 plt.savefig("crop_yield_wheat.jpg")
-plt.show()
+# plt.show()
 plt.cla()
 
 # высота растений
@@ -34,26 +36,77 @@ plt.ylabel("Высота растений (см)")
 plt.title("Высота исследованных образцов пшеницы")
 plt.grid()
 plt.savefig("height_wheat.jpg")
-plt.show()
+# plt.show()
 plt.cla()
 
-# распределение частот высот
-plt.hist(list(crop_yeild), bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
+# еще попробуем отфильтровать nan-ы
+no_nan_df = df_2[df_2["Урожайность.зерна..г."].notnull() & df_2["Высота.растений..см"].notnull()]
+crop_yield_filtered = no_nan_df["Урожайность.зерна..г."]
+height_filtered = no_nan_df["Высота.растений..см"]
+
+# распределение показателей урожайности
+plt.hist(list(crop_yield_filtered), bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
 plt.xlabel("Номер образца")
 plt.ylabel("Частоты показателей урожайности (г)")
-plt.title("Распределение частот урожайности зерна с образцов пшеницы")
+plt.title("Распределение фильтрованных частот урожайности зерна с образцов пшеницы")
 plt.grid()
-plt.savefig("height_wheat_hist.jpg")
-plt.show()
+plt.savefig("plots/height_wheat_hist.jpg")
+# plt.show()
 plt.cla()
 
 # распределение частот урожайности
-plt.hist(list(height))
+plt.hist(list(height_filtered))
 plt.xlabel("Номер образца")
 plt.ylabel("Частота высот растений (см)")
-plt.title("Распределение высот образцов пшеницы")
+plt.title("Распределение фильтрованных высот образцов пшеницы")
 plt.grid()
-plt.savefig("crop_yield_wheat_hist.jpg")
-plt.show()
+plt.savefig("plots/crop_yield_wheat_hist.jpg")
+# plt.show()
+plt.cla()
+
+# далее визуализируем данные с учетом нормализации
+
+# первый метод нормализации ('фенотип' минус 'среднее' делить на 'дисперсию')
+# распределение частот высот
+plt.hist(list(data_standardization(crop_yield)), bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
+plt.xlabel("Номер образца")
+plt.ylabel("Частоты нормализованных показателей урожайности (г)")
+plt.title("Распределение нормализованных стандартно частот урожайности зерна с образцов пшеницы")
+plt.grid()
+plt.savefig("plots/filter_stand_norm_height_wheat_hist.jpg")
+# plt.show()
+plt.cla()
+
+# распределение частот урожайности
+plt.hist(list(data_standardization(height)))
+plt.xlabel("Номер образца")
+plt.ylabel("Частота нормализованных высот растений (см)")
+plt.title("Распределение нормализованных стандартно высот образцов пшеницы")
+plt.grid()
+plt.savefig("plots/filter_stand_norm_crop_yield_wheat_hist.jpg")
+# plt.show()
+plt.cla()
+
+# второй вариант нормализации (через rankdata из scipy.stats и логистрическую функцию)
+# здесь еще нужно отфильтровать nan-ы
+
+# распределение частот высот
+plt.hist(list(rank_based_transform(crop_yield_filtered)), bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
+plt.xlabel("Номер образца")
+plt.ylabel("Частоты нормализованных показателей урожайности (г)")
+plt.title("Распределение нормализованных логистически частот урожайности зерна с образцов пшеницы")
+plt.grid()
+plt.savefig("plots/filter_log_norm_height_wheat_hist.jpg")
+# plt.show()
+plt.cla()
+
+# распределение частот урожайности
+plt.hist(list(rank_based_transform(height_filtered)))
+plt.xlabel("Номер образца")
+plt.ylabel("Частота нормализованных высот растений (см)")
+plt.title("Распределение нормализованных логистически высот образцов пшеницы")
+plt.grid()
+plt.savefig("plots/filter_log_norm_crop_yield_wheat_hist.jpg")
+# plt.show()
 plt.cla()
 
